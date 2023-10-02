1e71eba6fda6055409e09e1a0a8cdbba428794ad
ichobotov <115221037+ichobotov@users.noreply.github.com>
2023-10-02 13:38:27 +0300

    Add files via upload

---
+++ check_files_by_errors.py
@@ -0,0 +1,97 @@
+import os
+import shutil
+
+
+comnav_fail = 0
+radio_fail = 0
+
+def file_reader(file):
+    with open(file, 'r') as f:
+        for line in f:
+            yield line
+
+
+try:
+    os.mkdir (os.path.join(os.path.dirname(__file__), 'problem_trials'))
+except FileExistsError:
+    shutil.rmtree(os.path.join(os.path.dirname(__file__), 'problem_trials'))
+    os.mkdir(os.path.join(os.path.dirname(__file__), 'problem_trials'))
+
+os.mkdir(os.path.join(os.path.dirname(__file__), 'problem_trials', 'failed_trials'))
+
+
+
+files_list = os.listdir(os.path.dirname(__file__))
+for i in range(len(files_list)-1,-1,-1):
+    if not 'console' in files_list[i]:
+        del files_list[i]
+
+files_sorted = sorted(files_list, key=lambda x: int(os.path.splitext(x)[0].replace('console','')))
+
+file_number = 0
+file_name = 'results.txt'
+
+while os.path.exists(os.path.abspath(file_name)):
+    file_number += 1
+    file_name = 'results.txt'+str(file_number)
+
+result_file = open(file_name, 'a')
+
+for file in files_sorted:
+    for line in file_reader(file):
+        if "Open error S" in line:
+            # print (f'"Open error S" is found in {file}'+'\n\r')
+            # result_file.write(f'"Open error S" is found in {file}'+'\n\r')
+            shutil.copy2(file, os.path.join(os.path.dirname(__file__), 'problem_trials', 'radio_'+file))
+            radio_fail += 1
+
+        if "Open error P" in line:
+            # print (f'"Open error P" is found in {file}'+'\n\r')
+            # result_file.write(f'"Open error P" is found in {file}'+'\n\r')
+            shutil.copy2(file, os.path.join(os.path.dirname(__file__), 'problem_trials', 'comnav_'+file))
+            comnav_fail += 1
+        if "runGnssScript file [/mnt/fw/comnav.sc]" in line:
+            comnav_fail = 0
+
+
+
+    if radio_fail == 1:
+        print (f'"Open error S" in {file} but eventually radio statred'+'\n\r')
+        result_file.write(f'"Open error S" in {file} but eventually radio statred'+'\n\r')
+        radio_fail = 0
+    elif radio_fail > 1:
+        print(f'"Radio did not start in {file}'  + '\n\r')
+        result_file.write(f'"Radio did not start in {file}'  + '\n\r')
+        radio_fail = 0
+        shutil.copy2(file, os.path.join(os.path.dirname(__file__),'problem_trials', 'failed_trials', 'radio_' + file))
+
+    # if comnav_fail != 0:
+    #     print(f'"Open error P" in {file} but eventually Comnav statred' + '\n\r')
+    #     result_file.write(f'"Open error P" in {file} but eventually Comnav statred' + '\n\r')
+    if comnav_fail != 0:
+        print(f'"Comnav did not start in {file}' + '\n\r')
+        result_file.write(f'"Comnav did not start in {file}' + '\n\r')
+        shutil.copy2(file, os.path.join(os.path.dirname(__file__), 'problem_trials', 'failed_trials', 'comnav_' + file))
+
+
+
+problem_files = len([x for x in os.listdir(os.path.join(os.path.dirname(__file__),'problem_trials')) if os.path.isfile(os.path.join(os.path.dirname(__file__),'problem_trials', x))])
+failed_files = len (os.listdir(os.path.join(os.path.dirname(__file__),'problem_trials', 'failed_trials')))
+total_files = len(files_sorted)
+
+
+print ('##############################################\n\r')
+print (f'Problem files: {problem_files} of {total_files} ---> {round((problem_files / total_files) * 100, 2)}%'+'\n\r')
+print (f'Failed files: {failed_files} of {total_files} ---> {round((failed_files / total_files) * 100, 2)}%'+'\n\r')
+print ('##############################################\n\r')
+result_file.write ('##############################################\n\r')
+result_file.write (f'Problem files: {problem_files} of {total_files} ---> {round((problem_files / total_files) * 100, 2)}%'+'\n\r')
+result_file.write (f'Failed files: {failed_files} of {total_files} ---> {round((failed_files / total_files) * 100, 2)}%'+'\n\r')
+result_file.write ('##############################################\n\r')
+
+
+
+
+
+
+
--- check_files_by_size.py
+++ check_files_by_size.py
@@ -22,7 +22,8 @@ for file in os.listdir(os.path.dirname(__file__)):
             comnav_fail += 1
     if 'radio_' in file:
         radio_files += 1
-        if os.path.getsize(file) < radio_size or os.path.getsize(file) > radio_size:
+        # if os.path.getsize(file) < radio_size or os.path.getsize(file) > radio_size:
+        if os.path.getsize(file) < radio_size:
             shutil.copy2(file, os.path.join(os.path.dirname(__file__), 'failed_trials'))
             radio_fail += 1
 
@@ -30,8 +31,8 @@ print (f'Failed comnav files: {comnav_fail} ---> {round((comnav_fail / comnav_fi
 print (f'Failed radio files: {radio_fail} ---> {round((radio_fail / radio_files) * 100, 2)}%')
 
 with open ('result.txt', 'w') as f:
-    f.write(f'Failed comnav files: {comnav_fail} ---> {round((comnav_fail / comnav_files) * 100, 2)}%'+'\n\r')
-    f.write(f'Failed radio files: {radio_fail} ---> {round((radio_fail / radio_files) * 100, 2)}%')
+    f.write(f'Failed comnav files: {comnav_fail} of {comnav_files}---> {round((comnav_fail / comnav_files) * 100, 2)}%'+'\n\r')
+    f.write(f'Failed radio files: {radio_fail} of {radio_files} ---> {round((radio_fail / radio_files) * 100, 2)}%')
 
 
 
